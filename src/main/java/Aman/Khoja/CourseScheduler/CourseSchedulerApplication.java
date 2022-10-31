08cc10f3299c2933759d711d36d21e5282f30564
Aman Khoja <aak171730@utdallas.edu>
2022-10-31 17:55:56 -0500

    add existing file to Git

---
+++ src/main/java/Aman/Khoja/CourseScheduler/CourseSchedulerApplication.java
@@ -0,0 +1,14 @@
+package Aman.Khoja.CourseScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSchedulerApplication {
+
+	//main application
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Aman/Khoja/CourseScheduler/CourseSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package Aman.Khoja.CourseScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
