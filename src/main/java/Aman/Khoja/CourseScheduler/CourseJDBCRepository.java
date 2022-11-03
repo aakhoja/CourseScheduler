4de98517b51c9ec8dc0e8d749b542b7962cc60a1
Aman Khoja <aak171730@utdallas.edu>
2022-11-03 00:05:01 -0500

    added SQL insert functionality using jdbcTemplate

---
+++ src/main/java/Aman/Khoja/CourseScheduler/CourseJDBCRepository.java
@@ -0,0 +1,23 @@
+package Aman.Khoja.CourseScheduler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public class CourseJDBCRepository {
+
+    @Autowired
+    private JdbcTemplate springJdbcTemplate;
+    private static String INSERT_QUERY =
+            """
+                    insert into course (id,name,author)
+                    values (1,'SprintBooot','Aman Khoja')
+                    
+                    """;
+
+    public void insert(){
+        springJdbcTemplate.update(INSERT_QUERY);
+    }
+}
---
+++ src/main/java/Aman/Khoja/CourseScheduler/CourseJdbcCommandLineRunner.java
@@ -0,0 +1,17 @@
+package Aman.Khoja.CourseScheduler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CourseJdbcCommandLineRunner implements CommandLineRunner {
+
+    @Autowired
+    private CourseJDBCRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        repository.insert();
+    }
+}
